39aceb639e63bf69298780aca6d14e732c3deb34
闪闪神教 <2860007029@qq.com>
2026-03-22 17:56:36 +0800

    登录注册2.1
    登录注册，存进本地，可跳转场景

--- Assets/Devion Games/Login System/Scripts/Runtime/LoginManager.cs
+++ Assets/Devion Games/Login System/Scripts/Runtime/LoginManager.cs
@@ -4,364 +4,244 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.Networking;
 
 namespace DevionGames.LoginSystem
 {
     public class LoginManager : MonoBehaviour
     {
-		private static LoginManager m_Current;
+        private static LoginManager m_Current;
 
-		/// <summary>
-		/// The LoginManager singleton object. This object is set inside Awake()
-		/// </summary>
-		public static LoginManager current
-		{
-			get
-			{
-				Assert.IsNotNull(m_Current, "Requires Login Manager.Create one from Tools > Devion Games > Login System > Create Login Manager!");
-				return m_Current;
-			}
-		}
+        public static LoginManager current
+        {
+            get
+            {
+                Assert.IsNotNull(m_Current, "Requires Login Manager.Create one from Tools > Devion Games > Login System > Create Login Manager!");
+                return m_Current;
+            }
+        }
 
-		/// <summary>
-		/// Awake is called when the script instance is being loaded.
-		/// </summary>
-		private void Awake()
-		{
-			if (LoginManager.m_Current != null)
-			{
-				if (LoginManager.DefaultSettings.debug)
-					Debug.Log("Multiple LoginManager in scene...this is not supported. Destroying instance!");
-				Destroy(gameObject);
-				return;
-			}
-			else
-			{
-				LoginManager.m_Current = this;
-				if(LoginManager.DefaultSettings.debug)
-					Debug.Log("LoginManager initialized.");
-
-			}
-		}
+        private void Awake()
+        {
+            if (LoginManager.m_Current != null)
+            {
+                if (LoginManager.DefaultSettings.debug)
+                    Debug.Log("Multiple LoginManager in scene...this is not supported. Destroying instance!");
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                LoginManager.m_Current = this;
+                if (LoginManager.DefaultSettings.debug)
+                    Debug.Log("LoginManager initialized.");
+            }
+        }
 
         private void Start()
         {
-			if (LoginManager.DefaultSettings.skipLogin)
-			{
-				if (LoginManager.DefaultSettings.debug)
-					Debug.Log("Login System is disabled...Loading "+ LoginManager.DefaultSettings.sceneToLoad+" scene.");
-				UnityEngine.SceneManagement.SceneManager.LoadScene(LoginManager.DefaultSettings.sceneToLoad);
-			}
-		}
-
+            // ❗ 保持原逻辑：如果跳过登录就进游戏
+            if (LoginManager.DefaultSettings.skipLogin)
+            {
+                if (LoginManager.DefaultSettings.debug)
+                    Debug.Log("Login System is disabled...Loading " + LoginManager.DefaultSettings.sceneToLoad + " scene.");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(LoginManager.DefaultSettings.sceneToLoad);
+            }
+            else
+            {
+                // ✅ 确保登录界面打开（关键）
+                EventHandler.Execute("OnLogout");
+            }
+        }
 
         [SerializeField]
-		private LoginConfigurations m_Configurations = null;
+        private LoginConfigurations m_Configurations = null;
 
-		/// <summary>
-		/// Gets the login configurations. Configurate it inside the editor.
-		/// </summary>
-		/// <value>The database.</value>
-		public static LoginConfigurations Configurations
-		{
-			get
-			{
-				if (LoginManager.current != null)
-				{
-					Assert.IsNotNull(LoginManager.current.m_Configurations, "Please assign Login Configurations to the Login Manager!");
-					return LoginManager.current.m_Configurations;
-				}
-				return null;
-			}
-		}
+        public static LoginConfigurations Configurations
+        {
+            get
+            {
+                if (LoginManager.current != null)
+                {
+                    Assert.IsNotNull(LoginManager.current.m_Configurations, "Please assign Login Configurations to the Login Manager!");
+                    return LoginManager.current.m_Configurations;
+                }
+                return null;
+            }
+        }
 
+        private static Default m_DefaultSettings;
+        public static Default DefaultSettings
+        {
+            get
+            {
+                if (m_DefaultSettings == null)
+                {
+                    m_DefaultSettings = GetSetting<Default>();
+                }
+                return m_DefaultSettings;
+            }
+        }
 
-		private static Default m_DefaultSettings;
-		public static Default DefaultSettings
-		{
-			get
-			{
-				if (m_DefaultSettings == null)
-				{
-					m_DefaultSettings = GetSetting<Default>();
-				}
-				return m_DefaultSettings;
-			}
-		}
+        private static UI m_UI;
+        public static UI UI
+        {
+            get
+            {
+                if (m_UI == null)
+                {
+                    m_UI = GetSetting<UI>();
+                }
+                return m_UI;
+            }
+        }
 
-		private static UI m_UI;
-		public static UI UI
-		{
-			get
-			{
-				if (m_UI == null)
-				{
-					m_UI = GetSetting<UI>();
-				}
-				return m_UI;
-			}
-		}
+        private static Notifications m_Notifications;
+        public static Notifications Notifications
+        {
+            get
+            {
+                if (m_Notifications == null)
+                {
+                    m_Notifications = GetSetting<Notifications>();
+                }
+                return m_Notifications;
+            }
+        }
 
-		private static Notifications m_Notifications;
-		public static Notifications Notifications
-		{
-			get
-			{
-				if (m_Notifications == null)
-				{
-					m_Notifications = GetSetting<Notifications>();
-				}
-				return m_Notifications;
-			}
-		}
+        private static Server m_Server;
+        public static Server Server
+        {
+            get
+            {
+                if (m_Server == null)
+                {
+                    m_Server = GetSetting<Server>();
+                }
+                return m_Server;
+            }
+        }
 
-		private static Server m_Server;
-		public static Server Server
-		{
-			get
-			{
-				if (m_Server == null)
-				{
-					m_Server = GetSetting<Server>();
-				}
-				return m_Server;
-			}
-		}
+        private static T GetSetting<T>() where T : Configuration.Settings
+        {
+            if (LoginManager.Configurations != null)
+            {
+                return (T)LoginManager.Configurations.settings.Where(x => x.GetType() == typeof(T)).FirstOrDefault();
+            }
+            return default(T);
+        }
 
-		private static T GetSetting<T>() where T : Configuration.Settings
-		{
-			if (LoginManager.Configurations != null)
-			{
-				return (T)LoginManager.Configurations.settings.Where(x => x.GetType() == typeof(T)).FirstOrDefault();
-			}
-			return default(T);
-		}
+        // ===========================
+        // 🟢 本地注册
+        // ===========================
+        public static void CreateAccount(string username, string password, string email)
+        {
+            if (LoginManager.current != null)
+            {
+                LoginManager.current.StartCoroutine(CreateAccountInternal(username, password, email));
+            }
+        }
 
+        private static IEnumerator CreateAccountInternal(string username, string password, string email)
+        {
+            if (LoginManager.DefaultSettings.debug)
+                Debug.Log("[CreateAccount] 本地注册: " + username);
 
+            string key = "USER_" + username;
 
-		public static void CreateAccount(string username, string password, string email)
-		{
-			if (LoginManager.current != null)
-			{
-				LoginManager.current.StartCoroutine(CreateAccountInternal(username, password, email));
-			}
-		}
+            if (PlayerPrefs.HasKey(key))
+            {
+                Debug.Log("[CreateAccount] ❌ 用户已存在");
+                EventHandler.Execute("OnFailedToCreateAccount");
+                yield break;
+            }
 
-		private static IEnumerator CreateAccountInternal(string username, string password, string email)
-		{
-			if (LoginManager.Configurations == null)
-			{
-				EventHandler.Execute("OnFailedToCreateAccount");
-				yield break;
-			}
-			if (LoginManager.DefaultSettings.debug)
-				Debug.Log("[CreateAccount]: Trying to register a new account with username: " + username + " and password: " + password + "!");
+            PlayerPrefs.SetString(key, password);
+            PlayerPrefs.SetString("EMAIL_" + username, email);
+            PlayerPrefs.Save();
 
-			WWWForm newForm = new WWWForm();
-			newForm.AddField("name", username);
-			newForm.AddField("password", password);
-			newForm.AddField("email", email);
+            Debug.Log("[CreateAccount] ✅ 注册成功（本地）");
 
-			using (UnityWebRequest www = UnityWebRequest.Post(LoginManager.Server.serverAddress + "/" + LoginManager.Server.createAccount, newForm)) {
-				yield return www.SendWebRequest();
-				if (www.isNetworkError || www.isHttpError)
-				{
-					Debug.Log(www.error);
-				}
-				else {
-					if (www.downloadHandler.text.Contains("true"))
-					{
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[CreateAccount] Account creation was successfull!");
-						EventHandler.Execute("OnAccountCreated");
-					}else {
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[CreateAccount] Failed to create account.");
-						EventHandler.Execute("OnFailedToCreateAccount");
-					}
-				}
-			}
-		}
+            EventHandler.Execute("OnAccountCreated");
+        }
 
-		/// <summary>
-		/// Logins the account.
-		/// </summary>
-		/// <param name="username">Username.</param>
-		/// <param name="password">Password.</param>
-		public static void LoginAccount(string username, string password)
-		{
-			if (LoginManager.current != null)
-			{
-				LoginManager.current.StartCoroutine(LoginAccountInternal(username, password));
-			}
-		}
+        // ===========================
+        // 🟢 本地登录
+        // ===========================
+        public static void LoginAccount(string username, string password)
+        {
+            if (LoginManager.current != null)
+            {
+                LoginManager.current.StartCoroutine(LoginAccountInternal(username, password));
+            }
+        }
 
-		private static IEnumerator LoginAccountInternal(string username, string password)
-		{
-			if (LoginManager.Configurations == null)
-			{
-				EventHandler.Execute("OnFailedToLogin");
-				yield break;
-			}
-			if (LoginManager.DefaultSettings.debug)
-				Debug.Log("[LoginAccount] Trying to login using username: " + username + " and password: " + password + "!");
+        private static IEnumerator LoginAccountInternal(string username, string password)
+        {
+            if (LoginManager.DefaultSettings.debug)
+                Debug.Log("[LoginAccount] 本地登录: " + username);
 
-			WWWForm newForm = new WWWForm();
-			newForm.AddField("name", username);
-			newForm.AddField("password", password);
+            string key = "USER_" + username;
 
+            if (!PlayerPrefs.HasKey(key))
+            {
+                Debug.Log("[LoginAccount] ❌ 用户不存在");
+                EventHandler.Execute("OnFailedToLogin");
+                yield break;
+            }
 
-			using (UnityWebRequest www = UnityWebRequest.Post(LoginManager.Server.serverAddress + "/" + LoginManager.Server.login, newForm))
-			{
-				yield return www.SendWebRequest();
-				if (www.isNetworkError || www.isHttpError)
-				{
-					Debug.Log(www.error);
-				}
-				else
-				{
-					if (www.downloadHandler.text.Contains("true"))
-					{
-						PlayerPrefs.SetString(LoginManager.Server.accountKey, username);
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[LoginAccount] Login was successfull!");
-						EventHandler.Execute("OnLogin");
-					}
-					else
-					{
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[LoginAccount] Failed to login.");
-						EventHandler.Execute("OnFailedToLogin");
-					}
-				}
-			}
-		}
+            string savedPassword = PlayerPrefs.GetString(key);
 
-		/// <summary>
-		/// Recovers the password.
-		/// </summary>
-		/// <param name="email">Email.</param>
-		public static void RecoverPassword(string email)
-		{
-			if (LoginManager.current != null)
-			{
-				LoginManager.current.StartCoroutine(RecoverPasswordInternal(email));
-			}
-		}
+            if (savedPassword == password)
+            {
+                PlayerPrefs.SetString(LoginManager.Server.accountKey, username);
 
-		private static IEnumerator RecoverPasswordInternal(string email)
-		{
-			if (LoginManager.Configurations == null)
-			{
-				EventHandler.Execute("OnFailedToRecoverPassword");
-				yield break;
-			}
-			if (LoginManager.DefaultSettings.debug)
-				Debug.Log("[RecoverPassword] Trying to recover password using email: " + email + "!");
+                Debug.Log("[LoginAccount] ✅ 登录成功（本地）");
 
-			WWWForm newForm = new WWWForm();
-			newForm.AddField("email", email);
+                EventHandler.Execute("OnLogin");
+            }
+            else
+            {
+                Debug.Log("[LoginAccount] ❌ 密码错误");
+                EventHandler.Execute("OnFailedToLogin");
+            }
+        }
 
-			using (UnityWebRequest www = UnityWebRequest.Post(LoginManager.Server.serverAddress + "/" + LoginManager.Server.recoverPassword, newForm))
-			{
-				yield return www.SendWebRequest();
-				if (www.isNetworkError || www.isHttpError)
-				{
-					Debug.Log(www.error);
-				}
-				else
-				{
-					if (www.downloadHandler.text.Contains("true"))
-					{
-						EventHandler.Execute("OnPasswordRecovered");
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[RecoverPassword] Password recovered successfull!");
-					}
-					else
-					{
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[RecoverPassword] Failed to recover password.");
-						EventHandler.Execute("OnFailedToRecoverPassword");
-					}
-				}
-			}
-		}
+        // ===========================
+        // ❌ 以下功能暂时禁用（本地模式）
+        // ===========================
 
-		/// <summary>
-		/// Resets the password.
-		/// </summary>
-		/// <param name="username">Username.</param>
-		/// <param name="password">Password.</param>
-		public static void ResetPassword(string username, string password)
-		{
-			if (LoginManager.current != null)
-			{
-				LoginManager.current.StartCoroutine(ResetPasswordInternal(username, password));
-			}
-		}
+        public static void RecoverPassword(string email)
+        {
+            Debug.Log("本地模式不支持找回密码");
+            EventHandler.Execute("OnFailedToRecoverPassword");
+        }
 
-		private static IEnumerator ResetPasswordInternal(string username, string password)
-		{
-			if (LoginManager.Configurations == null)
-			{
-				EventHandler.Execute("OnFailedToResetPassword");
-				yield break;
-			}
-			if (LoginManager.DefaultSettings.debug)
-				Debug.Log("[ResetPassword] Trying to reset password using username: " + username + " and password: " + password + "!");
+        public static void ResetPassword(string username, string password)
+        {
+            Debug.Log("本地模式不支持重置密码");
+            EventHandler.Execute("OnFailedToResetPassword");
+        }
 
-			WWWForm newForm = new WWWForm();
-			newForm.AddField("name", username);
-			newForm.AddField("password", password);
+        // ===========================
+        // 📧 邮箱验证（保留）
+        // ===========================
+        public static bool ValidateEmail(string email)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            System.Text.RegularExpressions.Match match = regex.Match(email);
 
-			using (UnityWebRequest www = UnityWebRequest.Post(LoginManager.Server.serverAddress + "/" + LoginManager.Server.resetPassword, newForm))
-			{
-				yield return www.SendWebRequest();
-				if (www.isNetworkError || www.isHttpError)
-				{
-					Debug.Log(www.error);
-				}
-				else
-				{
-					if (www.downloadHandler.text.Contains("true"))
-					{
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("[RecoverPassword] Password resetted!");
-						EventHandler.Execute("OnPasswordResetted");
-				
-					}
-					else
-					{
-						if (LoginManager.DefaultSettings.debug)
-							Debug.Log("Failed to reset password.");
-						EventHandler.Execute("OnFailedToResetPassword");
-					}
-				}
-			}
-		}
+            if (match.Success)
+            {
+                if (LoginManager.DefaultSettings.debug)
+                    Debug.Log("Email validation was successfull for email: " + email + "!");
+            }
+            else
+            {
+                if (LoginManager.DefaultSettings.debug)
+                    Debug.Log("Email validation failed for email: " + email + "!");
+            }
 
-		/// <summary>
-		/// Validates the email.
-		/// </summary>
-		/// <returns><c>true</c>, if email was validated, <c>false</c> otherwise.</returns>
-		/// <param name="email">Email.</param>
-		public static bool ValidateEmail(string email)
-		{
-			System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-			System.Text.RegularExpressions.Match match = regex.Match(email);
-			if (match.Success)
-			{
-				if (LoginManager.DefaultSettings.debug)
-					Debug.Log("Email validation was successfull for email: " + email + "!");
-			}
-			else
-			{
-				if (LoginManager.DefaultSettings.debug)
-					Debug.Log("Email validation failed for email: " + email + "!");
-			}
-
-			return match.Success;
-		}
-	}
+            return match.Success;
+        }
+    }
 }
